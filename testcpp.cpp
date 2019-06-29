a1200907cbe2909403e18225e08bb14e4eb5296f
Shoaib Ahmed <shoaib.ahmed@slock.it>
2019-06-29 23:51:54 +0200

    Compare with cvector

--- testcpp.cpp
+++ testcpp.cpp
@@ -1,16 +1,18 @@
 #include "cvec.h"
+#define LOGARITHMIC_GROWTH
+#include "cvector.h"
 #include <cstdio>
 #include <random>
 #include <vector>
 
-#define CVEC_MAX 100000000
+#define CVEC_MAX 100000
 
 typedef struct {
   int x;
   int y;
 } point_t;
 
-void test_cvec() {
+static void test_cvec() {
   point_t p;
   vec v;
   vec_init(&v, point_t, 1024);
@@ -22,13 +24,38 @@ void test_cvec() {
   }
   size_t vl = v.l;
   for (size_t i = 0; i < vl; ++i) {
-    vec_back(&v, &p);
-    //    printf("x = %d, y = %d\n", p.x, p.y);
-    vec_pop(&v);
+    //    vec_back(&v, &p);
+    //    //    printf("x = %d, y = %d\n", p.x, p.y);
+    //    vec_pop(&v);
+    vec_erase(&v, 0);
   }
+  if (vec_empty(&v))
+    printf("empty\n");
+  vec_free(&v);
 }
 
-void test_veccpp() {
+static void test_cvector() {
+  point_t *v = NULL;
+  point_t p;
+  srand(time(NULL));
+  for (int j = 0; j < CVEC_MAX; ++j) {
+    p.x = std::rand();
+    p.y = std::rand();
+    vector_push_back(v, p);
+  }
+  size_t vl = vector_size(v);
+  for (size_t i = 0; i < vl; ++i) {
+    //    vec_back(&v, &p);
+    //    //    printf("x = %d, y = %d\n", p.x, p.y);
+    //    vec_pop(&v);
+    vector_erase(v, 0);
+  }
+  if (vector_empty(v))
+    printf("empty\n");
+  vector_free(v);
+}
+
+static void test_veccpp() {
   point_t p;
   std::vector<point_t> v;
   srand(time(NULL));
@@ -39,11 +66,15 @@ void test_veccpp() {
   }
   size_t vl = v.size();
   for (size_t i = 0; i < vl; ++i) {
-    auto &p = v.back();
-    (void)p;
-    //    printf("x = %d, y = %d\n", p.x, p.y);
-    v.pop_back();
+    //    auto &p = v.back();
+    //    (void)p;
+    //    //    printf("x = %d, y = %d\n", p.x, p.y);
+    //    v.pop_back();
+    v.erase(v.begin());
   }
+
+  if (v.empty())
+    printf("empty\n");
 }
 
 int main() {
@@ -58,5 +89,11 @@ int main() {
   end = clock();
   time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
   printf("cvec = %f\n", time_spent);
+
+  begin = clock();
+  test_cvector();
+  end = clock();
+  time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("cvector = %f\n", time_spent);
   return 0;
 }
