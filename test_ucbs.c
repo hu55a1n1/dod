02bf9f837470acba092e6869ea14eb609718c74c
Shoaib Ahmed <sufialhussaini@gmail.com>
2019-08-07 00:55:26 +0530

    Modify memory layout

--- test_ucbs.c
+++ test_ucbs.c
@@ -3,12 +3,12 @@
 #include "ucbs.h"
 
 int main(void) {
-  ucbs_t(9) bs9 = {.bytes = {0}};
+  ucbs_t(16) bs9 = {.bytes = {0}};
   ucbs_init(bs9, 10);
   ucbs_print(bs9);
   printf("\n");
-  ucbs_t(9) bs8 = {.bytes = {0}};
-  ucbs_init_str(bs8, "101101011");
+  ucbs_t(16) bs8 = {.bytes = {0}};
+  ucbs_init_str(bs8, "00001000");
   ucbs_print(bs8);
   return 0;
 }
--- ucbs.h
+++ ucbs.h
@@ -17,15 +17,14 @@
 
 #define ucbs_init(b, u) do { \
   size_t _usz_ = sizeof(b.bytes); \
-  uint8_t* _bp_ = b.bytes; \
+  uint8_t* _bp_ = b.bytes + _usz_ - 1; \
   while(_usz_) { \
     for (int _i_ = 7; _i_ >= 0; _i_--) {\
       size_t _x_ = ((_usz_ - 1) * 8) + _i_;\
-      printf("-- [%lu]: %lu\n", _usz_, _x_);\
       if (BIT_CHECK(u, _x_)) \
         BIT_SET(*_bp_, _x_ % 8); \
         } \
-    _bp_++; \
+    _bp_--; \
     _usz_--; \
     } \
 } while(0)
@@ -33,12 +32,12 @@
 #define ucbs_init_str(b, str) do { \
   size_t _i_ = 0; \
   char *_s_ = &str[strlen(str) - 1]; \
-  uint8_t* _bp_ = b.bytes + sizeof(b.bytes) -1; \
+  uint8_t* _bp_ = b.bytes; \
   while(_s_ && (*_s_ != 0)) { \
     if (*_s_ == '1') BIT_SET(*_bp_, _i_); \
     _i_++; \
     if (_i_ >= 8) { \
-      _bp_--; \
+      _bp_++; \
       _i_ = 0; \
     } \
     _s_--;\
@@ -46,7 +45,7 @@
 } while(0)
 
 #define ucbs_print(b) do { \
-  for (size_t _i_ = 0; _i_ < sizeof(b.bytes); ++_i_) \
+  for (int _i_ = sizeof(b.bytes); _i_ >= 0; _i_--) \
     for (int _j_ = 7; _j_ >= 0; _j_--) \
       printf("%u", BIT_CHECK(b.bytes[_i_], _j_) ? 1 : 0); \
 } while(0)
