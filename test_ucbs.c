92229158e21db7aab7b900042fa819c83a051149
Shoaib Ahmed <sufialhussaini@gmail.com>
2019-08-08 20:10:30 +0530

    Define storage type and default to size_t

--- test_ucbs.c
+++ test_ucbs.c
@@ -4,14 +4,14 @@
 
 #define BS_ASSERT_SIZE(sz) do {\
   ucbs_t(sz) bs##sz = {.bytes = {0}};\
-  assert(sizeof(bs##sz.bytes) == UCBS_ADJ_SZ(sz));\
+  assert(sizeof(bs##sz.bytes) == (UCBS_ADJ_SZ(sz) * sizeof(ucbs_storage_t))); \
 } while (0)
 
-#define test_access_funcs(l, str, cnt, sz, any, none, all) do { \
+#define test_access_funcs(l, str, cnt, any, none, all) do { \
   ucbs_t(l) _bs_ = {.bytes = {0}}; \
   ucbs_init_str(_bs_, str); \
   assert(ucbs_count(_bs_) == cnt); \
-  assert(ucbs_size(_bs_) == sz); \
+  assert(ucbs_size(_bs_) == UCBS_ARR_SZ(_bs_) * UCBS_T_BIT); \
   assert(ucbs_any(_bs_) == any); \
   assert(ucbs_none(_bs_) == none); \
   assert(ucbs_all(_bs_) == all); \
@@ -31,32 +31,28 @@ static void test_size(void) {
 
 static void test_init(void) {
   ucbs_t(16) bs1 = {.bytes = {0}};
-  ucbs_init(bs1, 0U);
-  assert(bs1.bytes[0] == 0 && bs1.bytes[1] == 0);
   ucbs_init(bs1, 43690U); // 43690 == b1010101010101010
   for (int i = 0; i < 16; ++i)
     assert(ucbs_test(bs1, i) == ((i % 2) ? true : false));
 
   ucbs_t(16) bs2 = {.bytes = {0}};
-  ucbs_init_str(bs2, "0");
-  assert(bs2.bytes[0] == 0 && bs2.bytes[1] == 0);
   ucbs_init_str(bs2, "1010101010101010");
   for (int i = 0; i < 16; ++i)
     assert(ucbs_test(bs2, i) == ((i % 2) ? true : false));
 }
 
 static void test_access(void) {
-  test_access_funcs(16, "1111111100000000", 8, 16, true, false, false);
-  test_access_funcs(16, "1111111111111111", 16, 16, true, false, true);
-  test_access_funcs(16, "0", 0, 16, false, true, false);
-  test_access_funcs(8, "11111111", 8, 8, true, false, true);
-  test_access_funcs(8, "0000", 0, 8, false, true, false);
-  test_access_funcs(8, "10000000", 1, 8, true, false, false);
-  test_access_funcs(8, "1111111", 7, 8, true, false, false);
+  test_access_funcs(16, "1111111100000000", 8, true, false, false);
+  test_access_funcs(16, "1111111111111111", 16, true, false, false);
+  test_access_funcs(16, "0", 0, false, true, false);
+  test_access_funcs(8, "11111111", 8, true, false, false);
+  test_access_funcs(8, "0000", 0, false, true, false);
+  test_access_funcs(8, "10000000", 1, true, false, false);
+  test_access_funcs(8, "1111111", 7, true, false, false);
 }
 
 static void test_operations(void) {
-  ucbs_t(16) bs = {.bytes = {0}};
+  ucbs_t_decl(bs, 16);
   ucbs_init_str(bs, "0");
   ucbs_setall(bs);
   assert(ucbs_all(bs) == true);
--- ucbs.h
+++ ucbs.h
@@ -8,11 +8,15 @@
 #include <stdbool.h>
 #include <limits.h>
 
+typedef size_t ucbs_storage_t;
+#define UCBS_T_BIT (sizeof(ucbs_storage_t) * CHAR_BIT)
+#define UCBS_ARR_SZ(b) (sizeof(b.bytes)/sizeof(ucbs_storage_t))
+
 #define BIT_SET(a, b) ((a) |= (1ULL << (b)))
 #define BIT_CLEAR(a, b) ((a) &= ~(1ULL << (b)))
 #define BIT_FLIP(a, b) ((a) ^= (1ULL << (b)))
 #define BIT_CHECK(a, b) (!!((a) & (1ULL << (b))))
-#define UCBS_ADJ_SZ(l) ((l / 8) + ((l % 8) ? 1 : 0))
+#define UCBS_ADJ_SZ(l) ((l / UCBS_T_BIT) + ((l % UCBS_T_BIT) ? 1 : 0))
 
 #define ucbs_t(l) struct { \
   uint8_t bytes[UCBS_ADJ_SZ(l)]; \
@@ -20,12 +24,12 @@
 
 #define ucbs_init(b, u) do { \
   size_t _usz_ = sizeof(b.bytes); \
-  uint8_t* _bp_ = b.bytes + _usz_ - 1; \
+  ucbs_storage_t* _bp_ = b.bytes + _usz_ - 1; \
   while(_usz_) { \
-    for (int _i_ = 7; _i_ >= 0; _i_--) {\
-      size_t _x_ = ((_usz_ - 1) * 8) + _i_;\
+    for (int _i_ = UCBS_T_BIT - 1; _i_ >= 0; _i_--) {\
+      size_t _x_ = ((_usz_ - 1) * UCBS_T_BIT) + _i_;\
       if (BIT_CHECK(u, _x_)) \
-        BIT_SET(*_bp_, _x_ % 8); \
+        BIT_SET(*_bp_, _x_ % UCBS_T_BIT); \
         } \
     _bp_--; \
     _usz_--; \
@@ -35,11 +39,11 @@
 #define ucbs_init_str(b, str) do { \
   size_t _i_ = 0; \
   char *_s_ = &str[strlen(str) - 1]; \
-  uint8_t* _bp_ = b.bytes; \
+  ucbs_storage_t* _bp_ = b.bytes; \
   while(_s_ && (*_s_ != 0)) { \
     if (*_s_ == '1') BIT_SET(*_bp_, _i_); \
     _i_++; \
-    if (_i_ >= 8) { \
+    if (_i_ >= UCBS_T_BIT) { \
       _bp_++; \
       _i_ = 0; \
     } \
@@ -48,51 +52,52 @@
 } while(0)
 
 #define ucbs_print(b) do { \
-  for (int _i_ = sizeof(b.bytes) - 1; _i_ >= 0; _i_--) \
-    for (int _j_ = 7; _j_ >= 0; _j_--) \
+  for (int _i_ = UCBS_ARR_SZ(b) - 1; _i_ >= 0; _i_--) { \
+    for (int _j_ = UCBS_T_BIT - 1; _j_ >= 0; _j_--) \
       printf("%u", BIT_CHECK(b.bytes[_i_], _j_) ? 1 : 0); \
+      } \
 } while(0)
 
 // Helpers
 #define ucbs_equals(b1, b2) (sizeof(b1.bytes) == sizeof(b2.bytes) && !memcmp(b1.bytes, b2.bytes, sizeof(b1.bytes)))
 
 // Bit access
-#define ucbs_count(b) ucbs_count_(b.bytes, sizeof(b.bytes))
-#define ucbs_size(b) (sizeof(b.bytes) * 8)
-#define ucbs_test(b, pos) BIT_CHECK(b.bytes[pos/8], pos%8U)
-#define ucbs_any(b) ucbs_any_(b.bytes, sizeof(b.bytes))
-#define ucbs_none(b) (!ucbs_any_(b.bytes, sizeof(b.bytes)))
-#define ucbs_all(b) ucbs_all_(b.bytes, sizeof(b.bytes))
+#define ucbs_count(b) ucbs_count_(b.bytes, UCBS_ARR_SZ(b))
+#define ucbs_size(b) (UCBS_ARR_SZ(b) * UCBS_T_BIT)
+#define ucbs_test(b, pos) BIT_CHECK(b.bytes[pos/UCBS_T_BIT], pos%UCBS_T_BIT)
+#define ucbs_any(b) ucbs_any_(b.bytes, UCBS_ARR_SZ(b))
+#define ucbs_none(b) (!ucbs_any_(b.bytes, UCBS_ARR_SZ(b)))
+#define ucbs_all(b) ucbs_all_(b.bytes, UCBS_ARR_SZ(b))
 
 // Bit operations
-#define ucbs_set(b, pos) BIT_SET(b.bytes[pos/8], pos%8U)
-#define ucbs_setall(b) memset(b.bytes, UINT8_MAX, sizeof(b.bytes))
-#define ucbs_reset(b, pos) BIT_CLEAR(b.bytes[pos/8], pos%8U)
-#define ucbs_resetall(b) memset(b.bytes, 0, sizeof(b.bytes))
-#define ucbs_flip(b, pos) BIT_FLIP(b.bytes[pos/8], pos%8U)
+#define ucbs_set(b, pos) BIT_SET(b.bytes[pos/UCBS_T_BIT], pos%UCBS_T_BIT)
+#define ucbs_setall(b) memset(b.bytes, 0xff, sizeof(b.bytes))
+#define ucbs_reset(b, pos) BIT_CLEAR(b.bytes[pos/UCBS_T_BIT], pos%UCBS_T_BIT)
+#define ucbs_resetall(b) memset(b.bytes, 0, UCBS_ARR_SZ(b.bytes))
+#define ucbs_flip(b, pos) BIT_FLIP(b.bytes[pos/UCBS_T_BIT], pos%UCBS_T_BIT)
 #define ucbs_flipall(b) do { \
-  for (size_t _i_ = 0; _i_ < sizeof(b.bytes); ++_i_) \
-    for (size_t _j_ = 0; _j_ < 8; ++_j_) \
+  for (size_t _i_ = 0; _i_ < UCBS_ARR_SZ(b); ++_i_) \
+    for (size_t _j_ = 0; _j_ < UCBS_T_BIT; ++_j_) \
       BIT_FLIP(b.bytes[_i_], _j_); \
 } while (0)
 
-static inline size_t ucbs_count_(const uint8_t *bytes, size_t l) {
+static inline size_t ucbs_count_(const ucbs_storage_t *bytes, size_t l) {
   size_t count = 0;
   for (size_t i = 0; i < l; ++i)
-    for (size_t j = 0; j < 8; ++j)
+    for (size_t j = 0; j < UCBS_T_BIT; ++j)
       if (BIT_CHECK(bytes[i], j)) count++;
   return count;
 }
 
-static inline bool ucbs_any_(const uint8_t *bytes, size_t l) {
+static inline bool ucbs_any_(const ucbs_storage_t *bytes, size_t l) {
   for (size_t i = 0; i < l; ++i)
     if (bytes[i] != 0) return true;
   return false;
 }
 
-static inline bool ucbs_all_(const uint8_t *bytes, size_t l) {
+static inline bool ucbs_all_(const ucbs_storage_t *bytes, size_t l) {
   for (size_t i = 0; i < l; ++i)
-    for (size_t j = 0; j < 8; ++j)
+    for (size_t j = 0; j < UCBS_T_BIT; ++j)
       if (BIT_CHECK(bytes[i], j) != 1)
         return false;
   return true;
