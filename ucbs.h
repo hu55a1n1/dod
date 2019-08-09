f2cf2d8bc702291cec3d2710cf4fd618441f93c8
Shoaib Ahmed <sufialhussaini@gmail.com>
2019-08-09 17:48:17 +0530

    Refactor only
    rename macro arg b -> bs

--- ucbs.h
+++ ucbs.h
@@ -15,20 +15,20 @@
 // ucbs specific macros and types
 typedef size_t ucbs_storage_t;
 #define UCBS_T_BIT (sizeof(ucbs_storage_t) * CHAR_BIT)
-#define UCBS_ARR_SZ(b) (sizeof((b).bytes)/sizeof(ucbs_storage_t))
+#define UCBS_ARR_SZ(bs) (sizeof((bs).bytes)/sizeof(ucbs_storage_t))
 #define UCBS_ADJ_SZ(l) ((l / UCBS_T_BIT) + ((l % UCBS_T_BIT) ? 1 : 0))
 #define ucbs_t(l) struct { \
   ucbs_storage_t bytes[UCBS_ADJ_SZ(l)]; \
   size_t len; \
 }
 #define ucbs_t_default(l) {.bytes = {0}, .len = l}
-#define ucbs_t_decl(b, l) ucbs_t(l) b = ucbs_t_default(l)
-#define ucbs_t_decl_ptr(b, l) ucbs_t(l) *b = memcpy(malloc(sizeof(*b)), &(ucbs_t(l))ucbs_t_default(l), sizeof(*b))
+#define ucbs_t_decl(bs, l) ucbs_t(l) bs = ucbs_t_default(l)
+#define ucbs_t_decl_ptr(bs, l) ucbs_t(l) *bs = memcpy(malloc(sizeof(*bs)), &(ucbs_t(l))ucbs_t_default(l), sizeof(*bs))
 
 // Constructors
-#define ucbs_init(b, u) do { \
-  size_t _usz_ = UCBS_ARR_SZ(b); \
-  ucbs_storage_t* _bp_ = &(b).bytes[_usz_ - 1]; \
+#define ucbs_init(bs, u) do { \
+  size_t _usz_ = UCBS_ARR_SZ(bs); \
+  ucbs_storage_t* _bp_ = &(bs).bytes[_usz_ - 1]; \
   while(_usz_) { \
     for (int _i_ = UCBS_T_BIT - 1; _i_ >= 0; _i_--) { \
       size_t _x_ = ((_usz_ - 1) * UCBS_T_BIT) + _i_;\
@@ -39,10 +39,10 @@ typedef size_t ucbs_storage_t;
     _usz_--; \
     } \
 } while(0)
-#define ucbs_init_str(b, str) do { \
+#define ucbs_init_str(bs, str) do { \
   size_t _i_ = 0; \
   char *_s_ = &str[strlen(str) - 1]; \
-  ucbs_storage_t* _bp_ = (b).bytes; \
+  ucbs_storage_t* _bp_ = (bs).bytes; \
   while(_s_ && (*_s_ != 0)) { \
     if (*_s_ == '1') BIT_SET(*_bp_, _i_); \
     _i_++; \
@@ -55,45 +55,45 @@ typedef size_t ucbs_storage_t;
 } while(0)
 
 // Helpers
-#define ucbs_print(b) do { \
-  size_t _sz_ = UCBS_ARR_SZ(b); \
+#define ucbs_print(bs) do { \
+  size_t _sz_ = UCBS_ARR_SZ(bs); \
   for (size_t _i_ = 0; _i_ < _sz_; ++_i_) { \
     for (size_t _j_ = 0; _j_ < UCBS_T_BIT; ++_j_) { \
-      if (_i_ >= (_sz_ - 1) && _j_ >= (b).len) break; \
-      printf("%u", BIT_CHECK((b).bytes[_sz_ - _i_ - 1], UCBS_T_BIT - _j_ - 1) ? 1 : 0); \
+      if (_i_ >= (_sz_ - 1) && _j_ >= (bs).len) break; \
+      printf("%u", BIT_CHECK((bs).bytes[_sz_ - _i_ - 1], UCBS_T_BIT - _j_ - 1) ? 1 : 0); \
     } \
   } \
 } while(0)
 #define ucbs_equals(b1, b2) ucbs_equals_((b1).bytes, (b1).len, (b2).bytes, (b2).len, UCBS_ARR_SZ(b1))
 
 // Bit access
-#define ucbs_count(b) ucbs_count_((b).bytes, (b).len, UCBS_ARR_SZ(b))
-#define ucbs_size(b) (UCBS_ARR_SZ(b) * UCBS_T_BIT)
-#define ucbs_test(b, pos) BIT_CHECK((b).bytes[pos/UCBS_T_BIT], pos%UCBS_T_BIT)
-#define ucbs_any(b) ucbs_any_((b).bytes, (b).len, UCBS_ARR_SZ(b))
-#define ucbs_none(b) (!ucbs_any_((b).bytes, (b).len, UCBS_ARR_SZ(b)))
-#define ucbs_all(b) ucbs_all_((b).bytes, (b).len, UCBS_ARR_SZ(b))
+#define ucbs_count(bs) ucbs_count_((bs).bytes, (bs).len, UCBS_ARR_SZ(bs))
+#define ucbs_size(bs) (UCBS_ARR_SZ(bs) * UCBS_T_BIT)
+#define ucbs_test(bs, pos) BIT_CHECK((bs).bytes[pos/UCBS_T_BIT], pos%UCBS_T_BIT)
+#define ucbs_any(bs) ucbs_any_((bs).bytes, (bs).len, UCBS_ARR_SZ(bs))
+#define ucbs_none(bs) (!ucbs_any_((bs).bytes, (bs).len, UCBS_ARR_SZ(bs)))
+#define ucbs_all(bs) ucbs_all_((bs).bytes, (bs).len, UCBS_ARR_SZ(bs))
 
 // Bit operations
-#define ucbs_set(b, pos) BIT_SET((b).bytes[pos/UCBS_T_BIT], pos%UCBS_T_BIT)
-#define ucbs_setall(b) memset((b).bytes, 0xff, sizeof((b).bytes))
-#define ucbs_reset(b, pos) BIT_CLEAR((b).bytes[pos/UCBS_T_BIT], pos%UCBS_T_BIT)
-#define ucbs_resetall(b) memset((b).bytes, 0, sizeof((b).bytes))
-#define ucbs_flip(b, pos) BIT_FLIP((b).bytes[pos/UCBS_T_BIT], pos%UCBS_T_BIT)
-#define ucbs_flipall(b) do { \
-  size_t _sz_ = UCBS_ARR_SZ(b); \
+#define ucbs_set(bs, pos) BIT_SET((bs).bytes[pos/UCBS_T_BIT], pos%UCBS_T_BIT)
+#define ucbs_setall(bs) memset((bs).bytes, 0xff, sizeof((bs).bytes))
+#define ucbs_reset(bs, pos) BIT_CLEAR((bs).bytes[pos/UCBS_T_BIT], pos%UCBS_T_BIT)
+#define ucbs_resetall(bs) memset((bs).bytes, 0, sizeof((bs).bytes))
+#define ucbs_flip(bs, pos) BIT_FLIP((bs).bytes[pos/UCBS_T_BIT], pos%UCBS_T_BIT)
+#define ucbs_flipall(bs) do { \
+  size_t _sz_ = UCBS_ARR_SZ(bs); \
   for (size_t _i_ = 0; _i_ < _sz_; ++_i_) { \
     for (size_t _j_ = 0; _j_ < UCBS_T_BIT; ++_j_) { \
-      if (_i_ >= (_sz_ - 1) && _j_ >= (b).len) break; \
-      BIT_FLIP((b).bytes[_i_], _j_); \
+      if (_i_ >= (_sz_ - 1) && _j_ >= (bs).len) break; \
+      BIT_FLIP((bs).bytes[_i_], _j_); \
     } \
   } \
 } while (0)
 
 // Bitset operations
-#define ucbs_to_string(b) ucbs_to_string_(b.bytes, (b).len, UCBS_ARR_SZ(b))
-#define ucbs_to_ulong(b) ucbs_to_ulong_(b.bytes)
-#define ucbs_to_ullong(b) ucbs_to_ullong_(b.bytes)
+#define ucbs_to_string(bs) ucbs_to_string_(bs.bytes, (bs).len, UCBS_ARR_SZ(bs))
+#define ucbs_to_ulong(bs) ucbs_to_ulong_(bs.bytes)
+#define ucbs_to_ullong(bs) ucbs_to_ullong_(bs.bytes)
 
 // Internal functions
 static inline bool
